c797592157080093adcc465d291a8e505d4540f1
Gabriel Felipetto <gfelipetto@gmail.com>
2024-10-22 17:35:09 -0300

    Update Seller and Custom Service Exception

---
+++ SalesWebMvc/Services/Exceptions/NotFoundExceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundExceptions : ApplicationException
+    {
+        public NotFoundExceptions(string message) : base(message)
+        {
+        }
+    }
+}
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 using System.Xml.Serialization;
 
 namespace SalesWebMvc.Services
@@ -18,7 +19,8 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
-        public Seller FindById(int id) {
+        public Seller FindById(int id)
+        {
             return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Insert(Seller seller)
@@ -26,10 +28,30 @@ namespace SalesWebMvc.Services
             _context.Add(seller);
             _context.SaveChanges();
         }
-        public void Remove(int id) {
+        public void Remove(int id)
+        {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller seller)
+        {
+            if (!_context.Seller.Any(x => x.Id == seller.Id))
+            {
+                throw new NotFoundExceptions("Id not found");
+            }
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+
+            }
+            catch (DbConcurrencyException e)
+            {
+
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
